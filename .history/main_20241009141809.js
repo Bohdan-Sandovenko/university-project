9403e7c3e7f26b748015e715ee665fe37bfbb224
Bohdan-Sandovenko <bogdaofficiall@gmail.com>
2024-10-10 12:10:07 +0200

    Initial commit

---
+++ .history/main_20241009141809.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const errorMsg = document.querySelector('p.error');
+const cityName = document.querySelector('h2.city');
+const weatherImg = document.querySelector('img.weather_img');
+const temp = document.querySelector('p.temp');
+const description = document.querySelector('p.description');
+const
